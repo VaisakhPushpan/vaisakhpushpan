aefb27e21143f0d934ce0e8146dd1aa1a5790be6
VaisakhPushpan <112495591+VaisakhPushpan@users.noreply.github.com>
2022-10-04 11:50:29 +0530

    Update dice.js

--- dice.js
+++ dice.js
@@ -22,20 +22,20 @@ if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
   setTimeout(()=>{
     document.querySelector("h1").innerHTML = "Refresh The Page";
-  },1000)
+  },2000)
 }
 else if (randomNumber2 > randomNumber1) {
   document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
   setTimeout(()=>{
     document.querySelector("h1").innerHTML = "Refresh The Page";
-  },1000)
+  },2000)
  
 }
 else {
   document.querySelector("h1").innerHTML = "Draw!";
   setTimeout(()=>{
     document.querySelector("h1").innerHTML = "Refresh The Page";
-  },1000)
+  },2000)
   
 }
 
